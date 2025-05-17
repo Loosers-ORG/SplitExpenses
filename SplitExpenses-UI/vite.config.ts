78406ec8d78fc6f81fa4e469a29d71601c64c3fb
Rohit Patil <74045753+rohitpatilweb@users.noreply.github.com>
2025-05-17 12:47:58 +0530

    Update vite.config.ts

--- SplitExpenses-UI/vite.config.ts
+++ SplitExpenses-UI/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173, // use Render's PORT
+  }
+});
+  
